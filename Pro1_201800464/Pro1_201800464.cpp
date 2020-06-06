16d51a80c568e248d545bb8db46ab51f6f31437c
PabloAndresArg <PabloAndresArg@gmail.com>
2020-06-06 00:12:01 -0600

    belachau
    :)

--- Pro1_201800464/Pro1_201800464.cpp
+++ Pro1_201800464/Pro1_201800464.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 #include"Init.h"
 #include"Usuario.h"
-#include "MatrixDis.h"
 #include"Matriz_dispersa.h"
 using namespace std;
 
@@ -16,22 +15,6 @@ using namespace std;
 int main()
 {
     //iniciar(); 
-   
-  /*
-    MatrixDis* m = new MatrixDis(); 
-    Usuario* use = new Usuario("pablo"); 
-    Usuario* use2 = new Usuario("val");
-    Usuario* use3 = new Usuario("peco");
-    Usuario* use4 = new Usuario("Espino");
-    Usuario* use5 = new Usuario("Rambo");
-    m->add("MAX", "SOLOLA", use5);
-    m->add("adidas", "REU", use);
-    m->add("adidas", "HUEHUETENANGO", use2);
-    m->add("ESTANCIA", "HUEHUETENANGO", use3);
-    m->add("MAX", "REU", use4);
-    //m->add("adidas", "SOLOLA", use3);
-    m->imprimirMatriz(); 
-    m->getGraphviz(); */
 
 
     
@@ -40,10 +23,15 @@ int main()
     matrix->add("adidas", "REU", "val");
     matrix->add("adidas", "HUEHUETENANGO", "Espino");
     matrix->add("ESTANCIA", "HUEHUETENANGO", "Rambo");
-
+    matrix->add("BURGER" , "PETEN" , "pio");
+    matrix->add("CAMPERO" , "PETEN", "ALI");
+    matrix->add("MACDONALS", "PETEN", "simon");
+    matrix->add("FIFA", "REU", "Ronaldo");
     matrix->add("MAX", "REU", "Espino");
-
     matrix->add("MAX", "SOLOLA", "pablo");
+    matrix->add("MAX", "PETEN", "JESUS");
+    matrix->add("BURGER", "HUEHUETENANGO", "CARLOS");
+    matrix->add("MAX", "HUEHUETENANGO", "LIO");
     matrix->getGraphviz();
    
 
--- Pro1_201800464/Rep.h
+++ Pro1_201800464/Rep.h
@@ -3,7 +3,7 @@
 #include<string>
 #include<string.h>
 #include<fstream>
-#include"NodoMatrix.h"
+
 #include"nMatrix.h"
 /*
 author: PABLO ARGUETA 
@@ -13,100 +13,7 @@ using namespace std;
 class Rep {
 
 public: 
-	void grafica_MATRIX(NodoMatrix* root) {
-		if (root->getDer() == NULL) {
-			cout << "MATRIZ VACIA" << endl;
-		}
-		else {
-			ofstream w;
-			w.open("REPORTES\\matriz.txt", ios::out);
-			if (w.fail())
-			{
-				cout << "NO SE ABRE :(" << endl;
-				system("pause");
-				exit(1);
-			}
-			w << "digraph G {bgcolor = black; \nnode[pin = true , shape = box, fontcolor = black  , fillcolor = gray  , width=0.7 ,style = filled] ;\ncharset = latin1;\n style = filled ;\nnodesep = 1.0;\n";
-			NodoMatrix* aux = root;
-			NodoMatrix* aux2 = NULL;
-			while (aux != NULL) {
-				if (aux->getDer() != NULL) {
-					w << "VAL_" << &*aux << "->" << "VAL_" << &*aux->getDer() << " [ dir=both ,color = white]; " << "\n";
-				}
-				if (aux->getIzq() != NULL) {
-					//w << "VAL_" << &*aux << "->" << "VAL_" << &*aux->getIzq() << " [arrowhead = empty, color = white]; " << "\n";
-				}
-				if (aux->getDown() != NULL) {
-					w << "VAL_" << &*aux << "->" << "VAL_" << &*aux->getDown() << " [  dir=both,  color = white]; " << "\n";
-				}
-				if (aux->getUp() != NULL) {
-					//w << "VAL_" << &*aux << "->" << "VAL_" << &*aux->getUp() << " [arrowhead = empty, color = white]; " << "\n";
-				}
-				// nomnbre-----------------------------------------------------------
-				if (aux != root && aux->getPos_x() == -1) {
-					w << "VAL_" << &*aux << "[label=\"" << aux->getEmpresa() << "\" , group = " << to_string(aux->getPos_x()) << ",pos = \"" << to_string(aux->getPos_x()) << "," << to_string((-1) * (aux->getPos_y())) << "\" ];\n";
-				}
-				else {
-					w << "VAL_" << &*aux << "[label = \"ROOT\", group = " << to_string(aux->getPos_x()) << ",pos = \"" << to_string(aux->getPos_x()) << "," << to_string((-1) * (aux->getPos_y())) << "\" ];\n";
-				}
-				if (aux->getDer() != NULL)
-				{
-					aux2 = aux->getDer();
-					while (aux2 != NULL)
-					{
-						if (aux2->getDer() != NULL) {
-							w << "VAL_" << &*aux2 << "->" << "VAL_" << &*aux2->getDer() << " [ dir=both, color = white]; " << "\n";
-						}
-						if (aux2->getIzq() != NULL) {
-							//	w << "VAL_" << &*aux2 << "->" << "VAL_" << &*aux2->getIzq() << " [arrowhead = empty, color = white]; " << "\n";
-						}
-						if (aux2->getDown() != NULL) {
-							w << "VAL_" << &*aux2 << "->" << "VAL_" << &*aux2->getDown() << " [ dir=both, color = white]; " << "\n";
-						}
-						if (aux2->getUp() != NULL) {
-							//	w << "VAL_" << &*aux2 << "->" << "VAL_" << &*aux2->getUp() << " [arrowhead = empty, color = white]; " << "\n";
-						}
-						//-------------------------------------------- NOMBRES
-						if (aux2->getPos_y() == -1) {// si mi pos en x es negativa en y si voy a tener 
-							w << "VAL_" << &*aux2 << "[label=\"" << aux2->getDepartamento() << "\", group = " << to_string(aux2->getPos_x()) << ",pos = \"" << to_string(aux2->getPos_x()) << "," << to_string((-1) * (aux2->getPos_y())) << "\" ];\n";
-						}
-						else {
-								w << "VAL_" << &*aux2 << "[ label = \"" << aux2->getUsuario()<< "\" ,dir=both ,fillcolor = chartreuse1, group = " << to_string(aux2->getPos_x()) << ",pos = \"" << to_string(aux2->getPos_x()) << "," << to_string((-1) * (aux2->getPos_y())) << "\" ];\n";
-						}
-						aux2 = aux2->getDer();
-					}
-				}
-				aux = aux->getDown();
-			}
 
-
-			// rank
-			aux = root;
-			aux2 = NULL;
-			while (aux != NULL) {
-				w << "{rank = same; ";
-				w << "VAL_" << &*aux << " ";
-				if (aux->getDer() != NULL)
-				{
-					aux2 = aux->getDer();
-					while (aux2 != NULL)
-					{
-						w << "VAL_" << &*aux2 << " ";
-						aux2 = aux2->getDer();
-					}
-				}
-				w << "};\n\n";
-				aux = aux->getDown();
-			}
-
-			w << "}\n";
-			w.close();
-			char eje[] = "dot -Tjpg REPORTES\\matriz.txt -o REPORTES\\matriz.jpg";
-			system(eje);
-			char abre[] = "REPORTES\\matriz.jpg";
-			system(abre);
-		}
-	}
 
 
 
